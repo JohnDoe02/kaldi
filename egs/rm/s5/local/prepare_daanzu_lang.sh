e9ed98a01171e647a13a22bb8d1e9ff1eea56801
criz <p1337@mailbox.org>
2020-09-08 23:02:27 +0200

    Added skript for generation of lang stuff

---
+++ egs/rm/s5/local/prepare_daanzu_lang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p data/local/lang
+
+echo "!OOV OOV" > data/local/lang/lexicon.txt
+cat kaldi_model/lexicon.txt >> data/local/lang/lexicon.txt
+
+echo "!OOV 1.0 OOV" > data/local/lang/lexiconp.txt
+cat kaldi_model/lexiconp.txt >> data/local/lang/lexiconp.txt
+
+cut -d ' ' -f 2- kaldi_model/lexicon.txt | sed 's/ /\n/g' | awk '{ if($1 != "SIL") print $1; }' | sort -u > data/local/lang/nonsilence_phones.txt
+echo SIL > data/local/lang/silence_phones.txt
+echo OOV >> data/local/lang/silence_phones.txt
+echo SIL > data/local/lang/optional_silence.txt
+
+utils/prepare_lang.sh data/local/lang '!OOV' data/local/ data/lang
